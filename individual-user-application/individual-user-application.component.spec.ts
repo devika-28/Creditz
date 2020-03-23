47597718db43b90ebf920fad135aa34a8b1bcd1b
Devika Bihani <41909437+devika-28@users.noreply.github.com>
2020-03-23 13:55:07 +0530

    Add files via upload

---
+++ individual-user-application/individual-user-application.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IndividualUserApplicationComponent } from './individual-user-application.component';
+
+describe('IndividualUserApplicationComponent', () => {
+  let component: IndividualUserApplicationComponent;
+  let fixture: ComponentFixture<IndividualUserApplicationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ IndividualUserApplicationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IndividualUserApplicationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ individual-user-application/individual-user-application.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { IndividualApplication } from '../model/individual-application'
+import { IndividualApplicationService } from '../services/individual-application.service'
+
+@Component({
+  selector: 'app-individual-user-application',
+  templateUrl: './individual-user-application.component.html',
+  styleUrls: ['../login/login.component.css']
+})
+export class IndividualUserApplicationComponent implements OnInit {
+
+  applicationModel = new IndividualApplication("", 0, 0, "", "", 0, 0, 0);
+  store = window.sessionStorage.getItem('userId');
+  storeRole = window.sessionStorage.getItem('role');
+
+  constructor(private applicationService: IndividualApplicationService) { }
+
+  ngOnInit(): void {
+  }
+
+  apply(){
+    window.alert("We will use the information submitted by you at the registration as your contact details");
+    this.applicationService.applyService(this.applicationModel.age,this.applicationModel.bankruptcy,this.applicationModel.criminalRecord,this.applicationModel.gender,this.applicationModel.loanAmount,this.applicationModel.loanTenure,this.applicationModel.occupation,this.applicationModel.pancard,this.applicationModel);
+  }
+
+}
