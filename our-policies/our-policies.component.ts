49ee0fc51146455826c9168f7391d8fa91dc9ff1
Devika Bihani <41909437+devika-28@users.noreply.github.com>
2020-03-23 13:50:34 +0530

    Add files via upload

---
+++ our-policies/our-policies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-policies',
+  templateUrl: './our-policies.component.html',
+  styleUrls: ['./our-policies.component.css']
+})
+export class OurPoliciesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
