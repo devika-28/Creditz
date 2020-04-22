b30638d2307ff4ffde76f545a4a0e2d6b016b26c
devika-28 <devikabihani@gmail.com>
2020-04-22 12:37:00 +0530

    Editis

--- Creditz-Angular-src/app/header/header.component.ts
+++ Creditz-Angular-src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HistoryComponent } from '../history/history.component';
 import {MatDialog} from '@angular/material/dialog';
+import { HomeComponent } from '../home/home.component';
 
 
 @Component({
@@ -24,6 +25,10 @@ logout(){
   window.sessionStorage.removeItem('role');
   window.alert("You have been logged Out");
   window.location.reload()
+}
+
+openProfileDialog() {
+  const dialogRef = this.dialog.open(HomeComponent);
 }
 
 openDialog() {
