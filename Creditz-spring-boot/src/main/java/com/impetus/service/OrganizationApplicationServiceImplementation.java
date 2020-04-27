1d28d449e290aa5f2d865d9ee1406f6b92a265fc
shivani sen <shivitouchsky703@gmail.com>
2020-04-27 06:50:53 -0700

    sonar lint and changes

--- Creditz-spring-boot/src/main/java/com/impetus/service/OrganizationApplicationServiceImplementation.java
+++ Creditz-spring-boot/src/main/java/com/impetus/service/OrganizationApplicationServiceImplementation.java
@@ -3,7 +3,6 @@ package com.impetus.service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -21,6 +20,13 @@ import com.impetus.repository.OrganizationRepository;
 public class OrganizationApplicationServiceImplementation implements OrganizationApplicationService {
 
     static final String APPROVED = "Approved";
+    
+    static final String DISAPPROVED = "Rejected";
+ 	static final String REJECTEDLOWCREDITS = "Rejected Low Credits";
+ 	static final String REJECTED = "Rejected";
+ 	static final String PENDING = "Pending";
+ 	static final String RECORDNOTFOUND = "Record Not Found";
+ 	static final String REJECTEDBADHISTORY = "Rejected Bad History";
     static final String EMAILSTATUS = "False";
     /** the organization application */
     @Autowired(required = true)
@@ -36,63 +42,82 @@ public class OrganizationApplicationServiceImplementation implements Organizatio
 
     /** @param application
      * @return Application id string and value of application id */
-    public Map<String, Long> organizationRiskMitigate(OrganizationApplicant application) {
+    public HashMap<String, Long> organizationRiskMitigate(OrganizationApplicant application) {
         HashMap<String, Long> json;
 
         try {
             CibilReport reportOfCurrentUser = cibilReport.findByPanCard(application.getPancard());
-            application.setApplicationStatus(this.approveOrDisapprove(application.getBankruptcy(), application.getCriminalRecord(),
-                    reportOfCurrentUser.getCategory(), reportOfCurrentUser.getCreditScore(), application.getLoanAmount(),
+            application.setApplicationStatus(this.approveOrDisapprove(application.getBankruptcy(),
+            		application.getCriminalRecord(),reportOfCurrentUser.getCategory(),
+            		reportOfCurrentUser.getCreditScore(), application.getLoanAmount(),
                     reportOfCurrentUser.getCreditUtilization(), reportOfCurrentUser.getMonthlyIncome(),
-                    application.getLoanTenure(),
-                    reportOfCurrentUser.getAssetCost(), reportOfCurrentUser.getLiabilities()));
+                    application.getLoanTenure(),reportOfCurrentUser.getAssetCost(), 
+                    reportOfCurrentUser.getLiabilities()));
 
-        } catch (Exception e) {
+            } catch (Exception e) {
 
             application.setApplicationStatus("record not found");
 
-        }
+            }
 
-        Long userId = (application.getUserId()).getUserId();
-        Long organizatinId = organization.getOrganizationIdByUserId(userId);
+            Long userId = (application.getUserId()).getUserId();
+            Long organizatinId = organization.getOrganizationIdByUserId(userId);
 
-        application.setEmailStatus(EMAILSTATUS);
-        organizationApplication.insertApplication(application.getBankruptcy(), application.getBusinessAge(), application.getCriminalRecord(),
-                application.getEmployeeCount(), application.getLicenseNumber(), application.getLoanAmount(), application.getLoanTenure(),
-                application.getOrganizationType(), application.getPancard(), application.getRevenue(), application.getApplicationStatus(),
-                organizatinId, userId, application.getEmailStatus());
+            application.setEmailStatus(EMAILSTATUS);
+            organizationApplication.insertApplication(application.getBankruptcy(), application.getBusinessAge(),
+            		 application.getCriminalRecord(),application.getEmployeeCount(), application.getLicenseNumber(),
+            		 application.getLoanAmount(), application.getLoanTenure(),application.getOrganizationType(), 
+                     application.getPancard(), application.getRevenue(), application.getApplicationStatus(),
+                     organizatinId, userId, application.getEmailStatus());
 
-        json = new HashMap<>();
-        json.put("Application_Id", organizationApplication.getApplicationId());
-        return json;
+            json = new HashMap<>();
+            json.put("Application_Id", organizationApplication.getApplicationId());
+            return json;
 
     }
 
+    /**
+     * find organization applicant  by user id
+     * @param userId
+     * @return organization applicant
+     */
     public OrganizationApplicant findByUserId(long userId) {
         return findByUserId(userId);
 
     }
+    
+    /**
+     * approve or disapprove
+     * @param bankrupt
+     * @param criminal
+     * @param assetCategory
+     * @param cibilScore
+     * @param loanAmount
+     * @param creditUtilization
+     * @param monthlyIncome
+     * @param loanTenure
+     * @param assetCost
+     * @param liabilites
+     * @return value for application status approved or other .
+     */
 
     public String approveOrDisapprove(int bankrupt, int criminal, String assetCategory, int cibilScore, int loanAmount, float creditUtilization,
             int monthlyIncome, int loanTenure, float assetCost, float liabilites) {
-        if (bankrupt == 1 || criminal == 1) {
-            return "Rejected";
+    	
+        if (loanAmount>7500000 || bankrupt == 1 || criminal == 1) {
+            return REJECTED;
         }
 
-        else if (!(assetCategory.equals("STD") || !(assetCategory.equals("NPA")))) {
-            return "Rejected Bad History";
+        else if (!(assetCategory.equals("STD")) || !(assetCategory.equals("NPA"))||creditUtilization >= 60) {
+            return REJECTEDBADHISTORY;
         }
 
         else if (cibilScore < 500 && cibilScore >= 300) {
-            return "Rejected Low Credits";
+            return REJECTEDLOWCREDITS;
         }
 
         else if (cibilScore <= 900 && cibilScore >= 700) {
             return APPROVED;
-        }
-
-        else if (creditUtilization >= 60) {
-            return "Rejected Bad History";
         }
 
         else if (cibilScore < 700 && cibilScore >= 500) {
@@ -118,7 +143,7 @@ public class OrganizationApplicationServiceImplementation implements Organizatio
     @Override
     public List<OrganizationApplicant> getHistory(OrganizationApplicant userId) {
 
-        return (List<OrganizationApplicant>) organizationApplication.findByUserId((userId.getUserId()).getUserId());
+        return  organizationApplication.findByUserId((userId.getUserId()).getUserId());
     }
 
     /** get organization applications in particular page with no of records.
