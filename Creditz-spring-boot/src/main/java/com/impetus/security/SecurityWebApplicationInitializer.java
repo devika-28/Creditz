5d1c1e650b3e2ae7a318f3ff3478c935ae056e31
devika-28 <devikabihani@gmail.com>
2020-04-30 14:23:22 +0530

    Delete

--- Creditz-spring-boot/src/main/java/com/impetus/security/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.impetus.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/** Security web application initializer. */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
